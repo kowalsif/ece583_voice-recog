94a44596bc86803d296c78953efc12afcf056d16
Ian Kowalski <kowalsif@rose-hulman.edu>
2014-02-12 15:15:04 -0500

    basic setup started

---
+++ hidden_unit0.cpp
@@ -0,0 +1,35 @@
+#include "hidden_unit0"
+
+using namespace std;
+
+
+hidden_unit0::hidden_unit0(){
+	this.units = 1;
+}
+
+hidden_unit0::hidden_unit0(float weights[]){
+	this.weights* = &weights[0];
+	this.units = 1;
+}
+
+float hidden_unit0::f(float output){
+	//TODO
+}
+float hidden_unit0::net(float[] weights){
+	//TODO
+}
+
+void hidden_unit0::update(float value);{
+	//TODO
+
+}
+
+void hidden_unit0::save(){
+	//TODO
+
+}
+
+void hidden_unit0::load(){
+	//TODO
+
+}
---
+++ main.cpp
@@ -0,0 +1,13 @@
+#include <stdio>
+#include <stdlib>
+
+
+int main(int argc, char* argv[]){
+	if (argc < 2){
+		return -1;
+	}
+
+	int mode = atoi(argv[1]);
+
+
+}
