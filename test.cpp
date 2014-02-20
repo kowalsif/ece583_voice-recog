84412c3f989d9085ca84d45c41efa8179015c287
Thriron <thriron9@gmail.com>
2014-02-19 20:16:06 -0500

    can compile, still some minor errors in math

--- test.cpp
+++ test.cpp
@@ -31,21 +31,25 @@ int main(int argc, char* argv[]){
 }
 
 int hidden_unit0_input_test(){
-	/*int ret = 0;
-	float input[1];
+	int ret = 0;
+	float input[1], output;
 	input[0] = 1.0;
 	hidden_unit0 hidden0(1);
-	if((hidden0.net(input) - 0.731059) < 0.000001){
+	output = hidden0.net(input);
+	if((output - 0.731059) < 0.000001){
 		cout << "hidden_unit0_input_test part 1 - PASS\n";
 	} else {
 		cout << "hidden_unit0_input_test part 1 - FAIL\n";
+		cout << "expected "<< 0.731059 << " was " << output << "\n";
 		ret = 1;
 	}
 	input[0] = 0.0;
-	if(hidden0.net(input) == 0.5){
+	output = hidden0.net(input);
+	if(output == 0.5){
 		cout << "hidden_unit0_input_test part 2 - PASS\n";
 	} else {
 		cout << "hidden_unit0_input_test part 2 - FAIL\n";
+		cout << "expected "<< 0.5 << " was " << output << "\n";
 		ret = 1;
 	}
 	return ret;
@@ -53,20 +57,24 @@ int hidden_unit0_input_test(){
 
 int hidden_unit1_input_test(){
 	int ret = 0;
-	float input[1];
+	float input[1], output;
 	input[0] = 1.0;
 	hidden_unit1 hidden1(1);
-	if((hidden1.net(input) - 0.731059) < 0.000001){
+	output = hidden1.net(input);
+	if((output - 0.731059) < 0.000001){
 		cout << "hidden_unit1_input_test part 1 - PASS\n";
 	} else {
 		cout << "hidden_unit1_input_test part 1 - FAIL\n";
+		cout << "expected "<< 0.731059 << " was " << output << "\n";
 		ret = 1;
 	}
 	input[0] = 0.0;
-	if(hidden1.net(input) == 0.5){
+	output = hidden1.net(input);
+	if(output == 0.5){
 		cout << "hidden_unit1_input_test part 2 - PASS\n";
 	} else {
 		cout << "hidden_unit1_input_test part 2 - FAIL\n";
+		cout << "expected "<< 0.5 << " was " << output << "\n";
 		ret = 1;
 	}
 	return ret;
@@ -91,11 +99,9 @@ int output_unit_input_test(){
 		ret = 1;
 	}
 	return ret;
-	*/
 }
 
 void simple_network_test(){
-/*
 	hidden_unit0 layer1(1);
 	hidden_unit1 layer2(1);
 	output_unit layer3(1);
@@ -135,7 +141,6 @@ void simple_network_test(){
 		cout << "J = " << J << "\n";
 	}
 	cout << "simple test completed in " << count << " iterations.\n";
-*/
 }
 		
 	
